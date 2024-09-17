6d0a6b30e651b8c454b0f668b05ef2812a6a367d
Lewis Moten <lewismoten@gmail.com>
2024-09-17 14:04:05 -0400

    handle normalization better

--- scrape.js
+++ scrape.js
@@ -2,61 +2,60 @@
 // https://unicode.org/emoji/charts/full-emoji-list.html
 // and past results into ./emoji.js
 
-var emojiJs = Array.prototype.slice // convert collection to an array
-  .call(document.getElementsByTagName("tr"))
-  .map(t => t.innerText) // forget the DOM, lets get messy and grab the text
-  .filter(t => /^\d+/.test(t)) // looking for numbered rows
-  .map(
-    (t, i) =>
-      // let's parse out a quick and dirty `key: 'code',`
-      (t.split("\t")[t.split("\t").length - 1] || "")
-        // use simililar characters in lower-ascii
-        .normalize("NFKD")
-        // Spell it out
-        .replace(/[&]/g, "And")
-        // remove apostrophes
-        .replace(/['’]/g, "")
-        .replace(/: #/g, " hash")
-        .replace(/: \*/g, " asterisk")
-        .replace(/1st/g, " first")
-        .replace(/2nd/g, " second")
-        .replace(/3rd/g, " third")
-        // we only want letters and numbers for key names.
-        // change reset to underscore
-        .replace(/[^a-z\d]/gi, "_") +
-      ': "' +
-      t
-        .split("\t")[1] // second cell
-        .replace(/U\+/g, "\\u{") // open unicode
-        .replace(/ /g, "}") + // close multiple codes
-      '}"' // close final code, add comma for next key in object
-  )
-  .map(t =>
-    t
-      .split("_") // keys are similar to snake_case. Let's change to camelCase
-      .reduce(
-        (camel, hump) =>
-          hump.length === 0
-            ? camel
-            : camel + hump[0].toUpperCase() + hump.slice(1),
-        ""
-      )
-  )
-  .map(t => {
-    // Some names have capitalized words (OKButton, BACKArrow).
-    // Let's fix the camels head be "okButton"
-    let m = t.match(/^([A-Z]+)[A-Z][a-z]/);
-    return m === null ? t : t.replace(m[1], m[1].toLowerCase());
-  })
-  .map(t => {
-    // for unicode JavaScript, the hex should be lower-case.
-    let m = t.match(/\{([^}]+)\}/g);
-    return m === null
-      ? t
-      : m.reduce((tt, mm) => tt.replace(mm, mm.toLowerCase()), t);
-  })
-  .map(t => t[0].toLowerCase() + t.slice(1)) // Everyone starts out lowercase
-  .sort() // make changes easier to find
+var tr = Array.prototype.slice.call(document.getElementsByTagName("tr"));
+var trText = tr.map(tr => tr.innerText);
+var trNumberedText = trText.filter(text => /^\d+/.test(text));
+var trCellText = trNumberedText.map(text => text.split('\t'));
+var parseUnicodeEscape = text => text
+  .replace(/U\+/g, '\\u{')
+  .replace(/\s+|$/g, '}')
+  .toLowerCase();
+var parseWordsAndSpaces = text => {
+  return text.normalize("NFD") // decompose into characters + combining marks
+    .replace(/[\u0300-\u036f]/g, '') // remove combining marks
+    // Spell it out
+    .replace(/[&]/g, "And")
+    // remove apostrophes
+    .replace(/['\u2019\x60]/g, "")
+    .replace(/: #/g, " hash")
+    .replace(/: \*/g, " asterisk")
+    .replace(/1st/g, " first")
+    .replace(/2nd/g, " second")
+    .replace(/3rd/g, " third")
+    // we only want letters and numbers for key names.
+    // change reset to underscore
+    .replace(/[^a-z\d]/gi, "_")
+}
+
+var camelHead = text => {
+  if (/^[A-Z]+$/.test(text)) return text.toLowerCase();
+  // Some names have capitalized words (OKButton, BACKArrow, ZZZ).
+  // Let's fix the camels head be "okButton" or "zzz"
+  text = text.replace(/^([A-Z]+)[A-Z][a-z]/, (search, replace) => {
+    return replace.toLowerCase() + search.slice(-2)
+  });
+  return text.charAt(0).toLowerCase() + text.slice(1);
+}
+var camelCase = text =>
+  camelHead(text.split("_")
+    .reduce(
+      (camel, hump) =>
+        hump.length === 0
+          ? camel
+          : camel + hump.charAt(0).toUpperCase() + hump.slice(1),
+      ""
+    ));
+
+var parseKey = text => camelCase(parseWordsAndSpaces(text));
+
+var entries = trCellText.map(cells => [
+  parseKey(cells[cells.length - 1]),
+  parseUnicodeEscape(cells[1])
+]);
+//entries.sort((a, b) => a[0].localeCompare(b[0]));
+emojiJs = entries
+  .sort(([a], [b]) => a < b ? -1 : a > b ? 1 : 0)
+  .map(([key, value]) => `${key}: "${value}"`)
   .join(",\r\n  ");
 
 // setup rest of code template
